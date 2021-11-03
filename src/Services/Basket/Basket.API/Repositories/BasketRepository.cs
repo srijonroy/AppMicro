a2b10c079029c9ef4d6f7fd70872969916fd8266
srijonroy <srijon.roy16@gmail.com>
2021-11-03 19:26:26 +0530

    Added basket service repository pattern implementation

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utf8Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+        public async Task DeleteBasket(string userName)
+        {
+             await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return await JsonSerializer.
+                DeserializeAsync<ShoppingCart>(
+                new MemoryStream(Encoding.UTF8.GetBytes(basket)));
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonSerializer.ToJsonString(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
